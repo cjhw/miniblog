9738fc85616e51e30dd94262f5d03709fe36619b
cjhw <1784581164@qq.com>
2023-02-05 02:11:28 +0800

    feat: 返回错误码

--- internal/miniblog/miniblog.go
+++ internal/miniblog/miniblog.go
@@ -10,6 +10,8 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/cjhw/miniblog/internal/pkg/core"
+	"github.com/cjhw/miniblog/internal/pkg/errno"
 	"github.com/cjhw/miniblog/internal/pkg/log"
 	mw "github.com/cjhw/miniblog/internal/pkg/middleware"
 	"github.com/cjhw/miniblog/pkg/verflag"
@@ -86,14 +88,14 @@ func run() error {
 
 	// 注册 404 Handler.
 	g.NoRoute(func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"code": 10003, "message": "Page not found."})
+		core.WriteResponse(c, errno.ErrPageNotFound, nil)
 	})
 
 	// 注册 /healthz handler.
 	g.GET("/healthz", func(c *gin.Context) {
 		log.C(c).Infow("Healthz function called")
 
-		c.JSON(http.StatusOK, gin.H{"status": "ok"})
+		core.WriteResponse(c, nil, map[string]string{"status": "ok"})
 	})
 
 	// 创建 HTTP Server 实例
